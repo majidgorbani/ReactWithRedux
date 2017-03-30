618d4c590a6386c71722f777fd6872bdd13b17fb
Majid Gorbani <majid.gorbani@acando.com>
2017-03-30 16:06:04 +0200

    sds

--- src/course/CoursesPage.js
+++ src/course/CoursesPage.js
@@ -1,14 +1,40 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component{
+    constructor(props, context){
+        super(props, context);
+
+        this.state={
+            course: { title: "" }
+        };
+        this.onClickSave=this.onClickSave.bind(this);
+        this.onTitleChange=this.onTitleChange.bind(this);
+    }
+
+    onTitleChange(event){
+        const course=this.state.course;
+        course.title=event.target.value;
+        this.setState({course: course});
+    }
+    onClickSave(){
+        alert('this '+ this.state.course.title);
+    }
 
     render(){
         return(
             <div className="jumbotron">
                 <h1>Courses</h1>
-                this is courses
-            </div>
+                <h2>Add Courses</h2>
+                <input
+                    onChange={this.onTitleChange}
+                    value={this.state.title} />
 
+                <input
+                    type="submit"
+                    value="save"
+                    onClick={this.onClickSave} />
+               
+            </div>
         );
     }
 }
