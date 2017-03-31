a07ef8a58d2585e45eb37d3717b2fda490e6014f
Majid Gorbani <majid.gorbani@acando.com>
2017-03-31 16:11:08 +0200

    dfd

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
-import App from "./components/App";
-import HomePage from "./components/home/HomePage";
-import AboutPage from "./components/about/AboutPage";
-import CoursesPage from "./course/CoursesPage";
+import App          from "./components/App";
+import HomePage     from "./components/home/HomePage";
+import AboutPage    from "./components/about/AboutPage";
+import CoursesPage  from "./components/course/CoursesPage";
 
 
 export default(
