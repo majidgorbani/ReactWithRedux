a554b3366efe4aff666aa6bbdedb5d273d77253e
Majid Gorbani <Majid Gorbani>
2017-04-01 09:39:59 +0200

    dd

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,12 +2,10 @@ export default function courseReducer(state=[], action){
     switch(action.type){
         case 'CREATE_COURSE':
             return [...state,
-                Object.assign({}, action.course)
-            ];
+            Object.assign({},action.course)];
         
         default:
             return state;
-
     }
 
 }
